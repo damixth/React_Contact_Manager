48381c429f3679aba42f4fcb5778199d04422c8e
Damith <37-se-0005@kdu.ac.lk>
2022-07-19 22:10:28 +0530

    items list added

--- src/index.js
+++ src/index.js
@@ -5,12 +5,29 @@ import './index.css';
 // function Hello(){
 //   return <h1>Hello there!</h1>;
 // }
-class Hello extends React.Component {
-  render() {
-    return <h1>Hello world!</h1>;
-  }
-} 
-const el =  <Hello />;
+// class Hello extends React.Component {
+//   render() {
+//     return <h1>Hello world!</h1>;
+//   }
+// } 
+// const el =  <Hello />;
+
+function Item(props) {
+  return <div className="item">
+  <b>Name:</b> {props.name} <br />
+  <b>Price:</b> ${props.price}
+  </div>;
+}
+
+function App() {
+  return <div>
+    <Item name="Cheese" price="4.99" />
+    <Item name="Bread" price="1.5" />
+    <Item name="Ice cream" price="24" />
+  </div>;
+}
+
+const el =<App />
 
 ReactDOM.render(
   el,
